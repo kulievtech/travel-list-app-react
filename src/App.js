50de7a600facd2b5119f2a0650e28cb897605c57
Tohir Kuliev <kuliev.tech@gmail.com>
2023-12-17 15:08:37 -0500

    work on stats component for displaying correct messages

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export default function App() {
                 onDeleteItem={handleDeleteItem}
                 onToggleItem={handleToggleItem}
             />
-            <Stats />
+            <Stats items={items} />
         </div>
     );
 }
@@ -118,10 +118,27 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     );
 }
 
-function Stats() {
+function Stats({ items }) {
+    if (!items.length)
+        return (
+            <p className="stats">
+                <em>Start adding some items to your packing list 🚀</em>
+            </p>
+        );
+
+    const numItems = items.length;
+    const numPacked = items.filter((item) => item.packed).length;
+    const percentage = Math.round((numPacked / numItems) * 100);
+
     return (
         <footer className="stats">
-            <em>💼 You have X items on your list, you already packed X (X%)</em>
+            <em>
+                {percentage === 100
+                    ? "You got everything! Ready to go ✈️"
+                    : `💼 You have ${numItems} items on your list, you already packed ${numPacked} (${
+                          isNaN(percentage) === true ? 0 : percentage
+                      }%)`}
+            </em>
         </footer>
     );
 }
